4d7780bcb473878c5ac1488663830f39a0d4dcdb
zhiyuan feng <tiger.zhiyuanfeng@gmail.com>
2024-02-28 14:56:21 -0700

    Set up the table booking API

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 from rest_framework.decorators import api_view
 from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 
-from .models import MenuItem
-from .serializers import MenuItemSerializer
+from .models import MenuItem,Booking
+from .serializers import MenuItemSerializer,BookingSerializer
 
 # Create your views here.
 class IndexView(generics.ListAPIView):
@@ -18,3 +19,8 @@ class MenuItemsView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
